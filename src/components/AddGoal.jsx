7baad9cb1ce304e393106fa7ee2a90de077a694e
Louise Aldridge <louisealdridge88@gmail.com>
2025-02-13 13:57:36 +0000

    Adding CSS Grid and styled add block

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -4,8 +4,8 @@ export default function AddGoal({ renderGoalForm, goalData }) {
     const buttonText = goalData.length > 0 ? "Add another goal" : "Get started and create your first goal"
 
     return (
-        <div className="add-goal-block">
-            <button onClick={() => renderGoalForm()}> {buttonText}</button>
+        <div>
+            <button className="add-goal-btn" onClick={() => renderGoalForm()}> {buttonText}</button>
         </div>
     )
 }

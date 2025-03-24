ae17ded649a57954b6433423b2176047f37c83e7
Louise Aldridge <louisealdridge88@gmail.com>
2025-03-24 14:15:10 +0000

    Basic template for header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import "/src/styles/header.css"
 export default function Header() {
     return (
         <header>
-            <h1>Pinpoint</h1>
+            <div>
+                <h1>Pinpoint</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt metus vel est</p>
+            </div>
+            <p className="countdown"><span className="medium-bold-text">235 days</span> until 31 December 2025</p>
         </header>
     )
 }

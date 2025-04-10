7573e91ff2b5136685528e6f9abe4a5e176a1006
Louise Aldridge <louisealdridge88@gmail.com>
2025-04-10 13:43:50 +0100

    Updating size of add goal btn

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -6,8 +6,9 @@ export default function AddGoal({ renderGoalForm, goalData }) {
     const buttonText = goalData.length > 0 ? "Add another goal" : "Get started and create your first goal"
 
     return (
-        <div>
+        <>
+            <h2>Add new goal </h2>
             <button className="add-goal-btn" onClick={() => renderGoalForm()}> {buttonText}</button>
-        </div>
+        </>
     )
 }

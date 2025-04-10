33e1c6ed259f382d0fd6457dcb4c995b931ff6ac
Louise Aldridge <louisealdridge88@gmail.com>
2025-04-10 16:41:45 +0100

    Updating mobile responsiveness of side nav

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -6,9 +6,9 @@ export default function AddGoal({ renderGoalForm, goalData }) {
     const buttonText = goalData.length > 0 ? "Add another goal" : "Get started and create your first goal"
 
     return (
-        <>
+        <div className="side-nav-add">
             <h2>Add new goal </h2>
             <button className="add-goal-btn" onClick={() => renderGoalForm()}> {buttonText}</button>
-        </>
+        </div>
     )
 }

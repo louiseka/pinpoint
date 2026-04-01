38541219a144661b43af816e2c981670a6f5b521
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-01 14:31:16 +0100

    Updated duration of confettti

--- src/App.jsx
+++ src/App.jsx
@@ -168,6 +168,7 @@ function App() {
               width={width}
               height={height}
               recycle={false}
+              tweenDuration={2000}
               onConfettiComplete={() => {
                 setGoalComplete(false);
               }}

fdd6b05ebfba37f2e388989b09e8989a6b20c9e2
Louise Aldridge <louisealdridge88@gmail.com>
2026-04-01 11:26:51 +0100

    Removing date from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,6 @@
 import "/src/styles/header.css";
 
 export default function Header() {
-  const countdownDate = new Date("31 December 2025 23:59:59").getTime();
-  const todaysDate = new Date().getTime();
-  const timeLeft = countdownDate - todaysDate;
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-
   return (
     <header>
       <div>
@@ -16,9 +11,6 @@ export default function Header() {
           intentional throughout your journey.
         </p>
       </div>
-      <p className="countdown">
-        <span className="medium-bold-text">{days}</span> days until 31 December
-      </p>
     </header>
   );
 }
